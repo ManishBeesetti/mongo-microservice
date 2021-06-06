32e5a1fffa67967579755ee0a5a3e0b25338d4c2
Manish Beesetti <pavan.manish9@gmail.com>
2021-06-06 16:58:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -83,14 +83,16 @@ def login():
         doc = collection.find_one({"email":email})
         print("login" + str(doc))
         
+        summation = (password + str(doc['private_key'])).hexdigest()
+        print(summation)
+        
         print("\n** PRINTING DOC PASSWORD **"+str(doc['password']),end="\n")
-        print("** CRYPTIC **"+str(hashlib.sha256((password+doc['private_key'].encode('utf-8'))).hexdigest()))
+#         print("** CRYPTIC **"+str(hashlib.sha256((password+doc['private_key'].encode('utf-8'))).hexdigest()))
         
 #      Validation Steps
         
         #password private string summation
-        summation = (password + str(doc['private_key'])).hexdigest()
-        print(summation)
+        
         
         
         
